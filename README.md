16ed91ef2ccf9992e445865250d90f81205e86f6
hi20160616 <hi20160616@163.com>
2021-06-09 12:45:49 +0800

    add clone details and rename proto service name

--- README.md
+++ README.md
@@ -26,7 +26,33 @@ Just clone and run the command to replace all string in all files, that contains
 ```
 cd /path/to/your/folder
 sed -i 's/foo/bar/g' *
+sed -i '' 's/zaobao/reuters/g' *.mod *.md ./internal/*/* ./cmd/*/* ./configs/* ./api/*/*/*/*
 ```
+rm git
+```
+rm -rf .git
+```
+git init
+```
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/<username>/<repo>.git
+git push -u origin main
+```
+grpc generate
+```
+protoc -I "./" -I "../../../" \
+    --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    api/fetchnews/cna/v1/fetchnews.proto
+```
+go mod tidy
+```
+go mod tidy
+```
+
 Then, edit go files to fix new target:
 - modified:   configs/configs.json
 - modified:   internal/fetcher/article.go
--- api/fetchnews/dwnews/v1/fetchnews_grpc.pb.go
+++ api/fetchnews/dwnews/v1/fetchnews_grpc.pb.go
@@ -15,158 +15,158 @@ import (
 // Requires gRPC-Go v1.32.0 or later.
 const _ = grpc.SupportPackageIsVersion7
 
-// FetchDwnewsClient is the client API for FetchDwnews service.
+// FetchClient is the client API for Fetch service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
-type FetchDwnewsClient interface {
+type FetchClient interface {
 	ListArticles(ctx context.Context, in *v1.ListArticlesRequest, opts ...grpc.CallOption) (*v1.ListArticlesResponse, error)
 	GetArticle(ctx context.Context, in *v1.GetArticleRequest, opts ...grpc.CallOption) (*v1.Article, error)
 	SearchArticles(ctx context.Context, in *v1.SearchArticlesRequest, opts ...grpc.CallOption) (*v1.SearchArticlesResponse, error)
 }
 
-type fetchDwnewsClient struct {
+type fetchClient struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewFetchDwnewsClient(cc grpc.ClientConnInterface) FetchDwnewsClient {
-	return &fetchDwnewsClient{cc}
+func NewFetchClient(cc grpc.ClientConnInterface) FetchClient {
+	return &fetchClient{cc}
 }
 
-func (c *fetchDwnewsClient) ListArticles(ctx context.Context, in *v1.ListArticlesRequest, opts ...grpc.CallOption) (*v1.ListArticlesResponse, error) {
+func (c *fetchClient) ListArticles(ctx context.Context, in *v1.ListArticlesRequest, opts ...grpc.CallOption) (*v1.ListArticlesResponse, error) {
 	out := new(v1.ListArticlesResponse)
-	err := c.cc.Invoke(ctx, "/fetchnews.dwnews.v1.FetchDwnews/ListArticles", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/fetchnews.dwnews.v1.Fetch/ListArticles", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *fetchDwnewsClient) GetArticle(ctx context.Context, in *v1.GetArticleRequest, opts ...grpc.CallOption) (*v1.Article, error) {
+func (c *fetchClient) GetArticle(ctx context.Context, in *v1.GetArticleRequest, opts ...grpc.CallOption) (*v1.Article, error) {
 	out := new(v1.Article)
-	err := c.cc.Invoke(ctx, "/fetchnews.dwnews.v1.FetchDwnews/GetArticle", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/fetchnews.dwnews.v1.Fetch/GetArticle", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *fetchDwnewsClient) SearchArticles(ctx context.Context, in *v1.SearchArticlesRequest, opts ...grpc.CallOption) (*v1.SearchArticlesResponse, error) {
+func (c *fetchClient) SearchArticles(ctx context.Context, in *v1.SearchArticlesRequest, opts ...grpc.CallOption) (*v1.SearchArticlesResponse, error) {
 	out := new(v1.SearchArticlesResponse)
-	err := c.cc.Invoke(ctx, "/fetchnews.dwnews.v1.FetchDwnews/SearchArticles", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/fetchnews.dwnews.v1.Fetch/SearchArticles", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// FetchDwnewsServer is the server API for FetchDwnews service.
-// All implementations must embed UnimplementedFetchDwnewsServer
+// FetchServer is the server API for Fetch service.
+// All implementations must embed UnimplementedFetchServer
 // for forward compatibility
-type FetchDwnewsServer interface {
+type FetchServer interface {
 	ListArticles(context.Context, *v1.ListArticlesRequest) (*v1.ListArticlesResponse, error)
 	GetArticle(context.Context, *v1.GetArticleRequest) (*v1.Article, error)
 	SearchArticles(context.Context, *v1.SearchArticlesRequest) (*v1.SearchArticlesResponse, error)
-	mustEmbedUnimplementedFetchDwnewsServer()
+	mustEmbedUnimplementedFetchServer()
 }
 
-// UnimplementedFetchDwnewsServer must be embedded to have forward compatible implementations.
-type UnimplementedFetchDwnewsServer struct {
+// UnimplementedFetchServer must be embedded to have forward compatible implementations.
+type UnimplementedFetchServer struct {
 }
 
-func (UnimplementedFetchDwnewsServer) ListArticles(context.Context, *v1.ListArticlesRequest) (*v1.ListArticlesResponse, error) {
+func (UnimplementedFetchServer) ListArticles(context.Context, *v1.ListArticlesRequest) (*v1.ListArticlesResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method ListArticles not implemented")
 }
-func (UnimplementedFetchDwnewsServer) GetArticle(context.Context, *v1.GetArticleRequest) (*v1.Article, error) {
+func (UnimplementedFetchServer) GetArticle(context.Context, *v1.GetArticleRequest) (*v1.Article, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetArticle not implemented")
 }
-func (UnimplementedFetchDwnewsServer) SearchArticles(context.Context, *v1.SearchArticlesRequest) (*v1.SearchArticlesResponse, error) {
+func (UnimplementedFetchServer) SearchArticles(context.Context, *v1.SearchArticlesRequest) (*v1.SearchArticlesResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method SearchArticles not implemented")
 }
-func (UnimplementedFetchDwnewsServer) mustEmbedUnimplementedFetchDwnewsServer() {}
+func (UnimplementedFetchServer) mustEmbedUnimplementedFetchServer() {}
 
-// UnsafeFetchDwnewsServer may be embedded to opt out of forward compatibility for this service.
-// Use of this interface is not recommended, as added methods to FetchDwnewsServer will
+// UnsafeFetchServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to FetchServer will
 // result in compilation errors.
-type UnsafeFetchDwnewsServer interface {
-	mustEmbedUnimplementedFetchDwnewsServer()
+type UnsafeFetchServer interface {
+	mustEmbedUnimplementedFetchServer()
 }
 
-func RegisterFetchDwnewsServer(s grpc.ServiceRegistrar, srv FetchDwnewsServer) {
-	s.RegisterService(&FetchDwnews_ServiceDesc, srv)
+func RegisterFetchServer(s grpc.ServiceRegistrar, srv FetchServer) {
+	s.RegisterService(&Fetch_ServiceDesc, srv)
 }
 
-func _FetchDwnews_ListArticles_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Fetch_ListArticles_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(v1.ListArticlesRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(FetchDwnewsServer).ListArticles(ctx, in)
+		return srv.(FetchServer).ListArticles(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/fetchnews.dwnews.v1.FetchDwnews/ListArticles",
+		FullMethod: "/fetchnews.dwnews.v1.Fetch/ListArticles",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(FetchDwnewsServer).ListArticles(ctx, req.(*v1.ListArticlesRequest))
+		return srv.(FetchServer).ListArticles(ctx, req.(*v1.ListArticlesRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _FetchDwnews_GetArticle_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Fetch_GetArticle_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(v1.GetArticleRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(FetchDwnewsServer).GetArticle(ctx, in)
+		return srv.(FetchServer).GetArticle(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/fetchnews.dwnews.v1.FetchDwnews/GetArticle",
+		FullMethod: "/fetchnews.dwnews.v1.Fetch/GetArticle",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(FetchDwnewsServer).GetArticle(ctx, req.(*v1.GetArticleRequest))
+		return srv.(FetchServer).GetArticle(ctx, req.(*v1.GetArticleRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _FetchDwnews_SearchArticles_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Fetch_SearchArticles_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(v1.SearchArticlesRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(FetchDwnewsServer).SearchArticles(ctx, in)
+		return srv.(FetchServer).SearchArticles(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/fetchnews.dwnews.v1.FetchDwnews/SearchArticles",
+		FullMethod: "/fetchnews.dwnews.v1.Fetch/SearchArticles",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(FetchDwnewsServer).SearchArticles(ctx, req.(*v1.SearchArticlesRequest))
+		return srv.(FetchServer).SearchArticles(ctx, req.(*v1.SearchArticlesRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-// FetchDwnews_ServiceDesc is the grpc.ServiceDesc for FetchDwnews service.
+// Fetch_ServiceDesc is the grpc.ServiceDesc for Fetch service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
-var FetchDwnews_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "fetchnews.dwnews.v1.FetchDwnews",
-	HandlerType: (*FetchDwnewsServer)(nil),
+var Fetch_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "fetchnews.dwnews.v1.Fetch",
+	HandlerType: (*FetchServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
 			MethodName: "ListArticles",
-			Handler:    _FetchDwnews_ListArticles_Handler,
+			Handler:    _Fetch_ListArticles_Handler,
 		},
 		{
 			MethodName: "GetArticle",
-			Handler:    _FetchDwnews_GetArticle_Handler,
+			Handler:    _Fetch_GetArticle_Handler,
 		},
 		{
 			MethodName: "SearchArticles",
-			Handler:    _FetchDwnews_SearchArticles_Handler,
+			Handler:    _Fetch_SearchArticles_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
